f076a1fdcf9f28e2c62a6f5003d4701c106f9449
diskutant <diskutant@googlemail.com>
2017-05-05 02:25:44 +0200

    add watched runtime
    besides unwatched runtime and total runtime it now also shows the
    watched runtime

--- addon.py
+++ addon.py
@@ -14,7 +14,7 @@ def display_details(addon, show_title, message):
 
 if __name__ == '__main__':
     addon = xbmcaddon.Addon()
-    remaining_runtime, total_runtime = 0, 0
+    remaining_runtime, total_runtime, watched_runtime = 0, 0, 0
     query = {"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "params": { "filter": { "field": "title", "operator": "is", "value": "" }, "limits": { "start": 0, "end": 1}, "properties": [ "title", "originaltitle", "playcount", "episode", "episodeguide", "watchedepisodes", "season"], "sort": { "order": "ascending", "method": "label"} }, "id": "libTvShows"}
     query = json.loads(json.dumps(query))
     query['params']['filter']['value'] = sys.listitem.getLabel()
@@ -35,15 +35,19 @@ if __name__ == '__main__':
         for episode in response['result']['episodes']:
             if episode['playcount'] == 0:
                 remaining_runtime += episode['runtime'] / 60
+	    else:
+		watched_runtime += episode['runtime'] / 60
             total_runtime += episode['runtime'] / 60
         
         remaining_runtime = convert_time(remaining_runtime)
         total_runtime = convert_time(total_runtime)
+	watched_runtime = convert_time(watched_runtime)
         
         if addon.getSetting('detailed_info') == 'true':
             percent = '{0}%'.format(str(round((float(watched_episodes)/total_episodes) * 100))[:-2])
             message = 'Watched/Unwatched: {0}/{1} ({2})'.format(watched_episodes, total_episodes, percent)
             message = '{0}\nRemaining runtime: {1}'.format(message, remaining_runtime)
+            message = '{0}\nWatched runtime: {1}'.format(message, watched_runtime)
             message = '{0}\nTotal runtime: {1}'.format(message, total_runtime)
             display_details(addon, show_title, message)
         else:
